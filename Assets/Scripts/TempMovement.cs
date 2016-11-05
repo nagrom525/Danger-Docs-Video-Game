e912ccb1c9ba28bbc1dfc144199eb6368bea96b0
Alex Chojnacki <thealex@umich.edu>
2016-11-05 17:30:34 -0400

    added basicmovement script; add to game objects to controll with WASD

---
+++ Assets/Scripts/TempMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempMovement : MonoBehaviour {
+	public GameObject target;
+	public float speed = 3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Basic movement
+		if (Input.GetKey(KeyCode.A)) {
+			target.transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			target.transform.position += Vector3.back * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			target.transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.W)) {
+			target.transform.position += Vector3.forward * speed * Time.deltaTime;
+		}
+	}
+}
