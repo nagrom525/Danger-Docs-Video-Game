187bbba074085bbbe519779c1a84ca1e6d028e11
Alex Chojnacki <thealex@umich.edu>
2016-11-05 17:19:44 -0400

    Doctor Implementations - OnJoystickMovement, OnInteractionButtonPressed
    Setup priliminary code for OnInteractionButtonPress, which calls getNearbyInteractable.
    The doctor interacts with the nearest interactable using the DoctorIntitiatesInteracting
    function.

--- Assets/Scripts/Doctor.cs
+++ Assets/Scripts/Doctor.cs
@@ -5,9 +5,12 @@ public class Doctor : MonoBehaviour {
     // CurrentTool can only be set by the Doctor when it interacts with a tool
     public Tool curentTool {get; private set;}
 
+	// Radius of sphere for checking for interactiables.
+	private float nearbyInteractableRange = 3f;
+
 	// Use this for initialization
 	void Start () {
-	    // NEED to register event listner functions to the DoctorEvents singleton delegates
+	    // TODO: NEED to register event listner functions to the DoctorEvents singleton delegates
 	}
 	
 	// Update is called once per frame
@@ -20,5 +23,69 @@ public class Doctor : MonoBehaviour {
     public void OnJumpKeyPressed() {
 
     }
+
+	// Logic for receiving joystick movement.
+	// Moves the player according to the Vector3
+	// recieved from input manager.
+	public void OnJoystickMovement(Vector3 joystickVec) {
+		// We should never be moving in the z direction.
+		joystickVec.z = 0f;
+		// Move in the direction of the joystick.
+		pos += joystickVec * Time.deltaTime;
+	}
+
+
+	// When the interaction button is pressed, we must check to see if there
+	// is and interactiable nearby. If there is, then we send a message to
+	// the interactable that the doctor is initiating an interaction. The
+	// interactiable accepts the interaction message and acts on it only if
+	// it is valid.
+	public void OnInteractionButtonPressed() {
+		// May return null.
+		Interactable nearbyInteractable = getNearbyInteractable(nearbyInteractableRange);
+
+		// If there is a nearby interactable, then begin interacting!
+		if (nearbyInteractable != null) {
+			nearbyInteractable.DocterIniatesInteracting(this);
+		}
+	}
+
+	// Gets the nearest nearby interactable within sphere of radius "range".
+	private Interactable getNearbyInteractable(float range) {
+
+		// Get the interactables. Eventually, this should take a third agrument
+		// (layer mask) which ignores everything that isn't an interactable.
+		Collider[] interactiablesInRange = Physics.OverlapSphere(pos, range);
+		
+		// Setup linear search for nearest interactable.
+		Interactable nearestInteractable = null;
+		float runningNearestDistance = Mathf.Infinity;
+
+		for (int i = 0; i < interactiablesInRange.Length; i++) {
+			// Get Vector3 between pos of doctor and interactable
+			Vector3 interactablePos = interactiablesInRange[i].transform.position;
+			// Comparing sqrDistances is faster than mag. Avoids sqrt op.
+			float sqrDist = (interactablePos - pos).sqrMagnitude;
+			
+			// If this interactable is closer than the current closest, update
+			// to this one.
+			if (runningNearestDistance > sqrDist) { 
+				runningNearestDistance = sqrDist;
+				nearestInteractable = interactiablesInRange[i].gameObject.GetComponent<Interactable>();
+			}
+		}
+
+		return nearestInteractable;
+	}
+
+	// For my convenience
+	private Vector3 pos {
+		get {
+			return this.transform.position;
+		}
+		set {
+			this.transform.position = value;
+		}
+	}
 
 }
