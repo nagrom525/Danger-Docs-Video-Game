bf48992584d390fa8831fecc44050295fab624e3
Alex Chojnacki <thealex@umich.edu>
2016-11-06 19:42:41 -0500

    added patient heartbeat interval tracking

--- Assets/Scripts/Patient.cs
+++ Assets/Scripts/Patient.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class Patient : MonoBehaviour {
 
+
+	public float bpm;
+
+	private float last_beat_time;
+	private float next_beat_time;
+
 	// Use this for initialization
 	void Start () {
-	
+		// Dummy BPM for now.
+		bpm = 120f;
+
+		last_beat_time = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+
+
+		// if the time since the last heart beat has passed.
+		if (Time.time > next_beat_time) {
+			
+
+			// TODO: Add heartbeat message / vitals / things here.
+			// EX: renderHeartBeat();
+			print("Heartbeat Triggered.\nCurrent BPM: ", bpm);
+
+			// update last_beat_time
+			last_beat_time = Time.time;
+			next_beat_time = last_beat_time + bpmToSecondsInterval(bpm);
+		}
+	}
+
+
+	private float bpmToSecondsInterval(float bpm) {
+		return (1f / (bpm / 60f));
 	}
 }
