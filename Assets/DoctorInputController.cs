f51683ff280235c336244bbb47374f18f9e8790d
Benjamin Lapid <benlap@umich.edu>
2016-11-06 19:18:49 -0500

    Added controller support
    Up to 4 players can control individual doctors, try Debug_Ben scene

---
+++ Assets/DoctorInputController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class DoctorInputController : MonoBehaviour
+{
+    public Doctor doctor;
+    public int playerNum;
+    Renderer playerRenderer;
+    public Vector3 dir;
+    
+    // Use this for initialization
+    void Start()
+    {
+        playerRenderer = GetComponent<Renderer>();
+        doctor = GetComponent<Doctor>();
+    }
+
+    void Update()
+    {
+        InputDevice inputDevice = (InputManager.Devices.Count > playerNum) ? InputManager.Devices[playerNum] : null;
+        if (inputDevice == null)
+        {
+            // If no controller exists for this doctor make it translucent.
+            playerRenderer.material.color = new Color(1.0f, 1.0f, 1.0f, 0.2f);
+        }
+        else
+        {
+            UpdateWithInputDevice(inputDevice);
+        }
+    }
+
+    void UpdateWithInputDevice(InputDevice inputDevice)
+    {
+        // Set material color based on which action is pressed.
+        if (inputDevice.Action1)
+        {
+            playerRenderer.material.color = Color.green;
+
+            //Pickup
+            doctor.OnPickupButtonPressed();
+        }
+        else
+        if (inputDevice.Action2)
+        {
+            playerRenderer.material.color = Color.red;
+        }
+        else
+        if (inputDevice.Action3)
+        {
+            playerRenderer.material.color = Color.blue;
+
+            //Interact
+            doctor.OnInteractionButtonPressed();
+        }
+        else
+        if (inputDevice.Action4)
+        {
+            playerRenderer.material.color = Color.yellow;
+        }
+        else
+        {
+            playerRenderer.material.color = Color.white;
+        }
+
+        // Rotate target object with both sticks and d-pad.
+
+        var direction = 10.0f * new Vector3(inputDevice.Direction.X, 0, inputDevice.Direction.Y);
+        //transform.Translate(direction);
+        doctor.OnJoystickMovement(direction);
+    }
+}
