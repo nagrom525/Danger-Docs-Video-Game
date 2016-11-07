8c7b324f1edf677b340916f3bde5e7a58e7c8833
Morgan Erlich <nagrom525@gmail.com>
2016-11-06 20:48:02 -0500

    Game lost panel

---
+++ Assets/Scripts/MenuControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static void RestartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public static void ExitGame() {
+        Application.Quit();
+    }
+
+    public static void MainMenu() {
+        SceneManager.LoadScene(0);
+    }
+}
