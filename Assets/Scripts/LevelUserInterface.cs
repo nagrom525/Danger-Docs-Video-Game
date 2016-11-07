fda7508a96cf6d1b9db7ceed26faebe877377a43
elabraha <elabraha@umich.edu>
2016-11-06 22:08:49 -0500

    pushing ui changes

--- Assets/Scripts/LevelUserInterface.cs
+++ Assets/Scripts/LevelUserInterface.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Serialization;
 
 public class LevelUserInterface : MonoBehaviour {
-
+	public Text heartrate;
+	public Image EventSignal;
+	public float doctorBlinkDuration;
 	// Use this for initialization
 	void Start () {
 	    // We probably want to register private member functions with DoctorEvents delegates
