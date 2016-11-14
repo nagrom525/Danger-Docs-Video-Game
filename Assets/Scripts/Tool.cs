f01da4d596bde4429967445d9521abdec8ee52b6
Benjamin Lapid <benlap@umich.edu>
2016-11-14 00:25:29 -0500

    suture event

--- Assets/Scripts/Tool.cs
+++ Assets/Scripts/Tool.cs
@@ -4,7 +4,7 @@ using System.Collections;
 // base class for Tool, all tools derive from this class and must implement getToolType
 // also abstract fuctions can be implemented if there is shared code across all types of tool
 public abstract class Tool : MonoBehaviour {
-    public enum ToolType {NONE, TYPE_1, TYPE_2, TYPE_3, TYPE_4}
+    public enum ToolType {NONE, TYPE_1, TYPE_2, TYPE_3, TYPE_4, SCALPEL, GAUZE, FORCEPS, SUTURE}
 
     public abstract ToolType GetToolType();
 
