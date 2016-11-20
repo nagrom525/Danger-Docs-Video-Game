68bd1aedd608dd51831d5d2e0dff17dc5d56d03c
Alex Chojnacki <thealex@umich.edu>
2016-11-19 21:14:31 -0500

    Cleaned up project
    Updated doctor prefab, deleted unused tools and scripts, changed defibulator script and prefab.

---
+++ Assets/Scripts/Defibulator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Defibulator : Tool
+{
+    public override ToolType GetToolType()
+    {
+        return ToolType.DEFIBULATOR;
+    }
+
+	// 
+    public override void OnDoctorInitatedInteracting()
+    {
+		print("OnDoctorInitatedInteracting was called");
+    }
+
+    public override void OnDoctorTerminatedInteracting()
+    {
+        throw new NotImplementedException();
+    }
+}
