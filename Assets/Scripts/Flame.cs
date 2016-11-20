ca25855cb4b3f6377ea994207c15a6a96b30df8c
Morgan Erlich <nagrom525@gmail.com>
2016-11-20 10:30:49 -0500

    Started code fro flame

---
+++ Assets/Scripts/Flame.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flame : MonoBehaviour {
+    private enum Direction { NORTH, SOUTH, EAST, WEST};
+
+    public float fireGridScale = 1.0f;
+    public float timeFireSpawnDelay = 2.0f;
+
+    // flame graph connections
+    public delegate void ChildEvent();
+    public ChildEvent onChildDestroyed;
+    private Flame parent;
+
+    private float lastFireTime;
+    private Direction nextFireSpawnDirection;
+    
+
+
+    // Use this for initialization
+    void Start () {
+        lastFireTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setParent(Flame parent) {
+        this.parent = parent;
+        parent.onChildDestroyed += OnDestroy;
+    }
+
+    void OnDestroy() {
+
+    }
+}
