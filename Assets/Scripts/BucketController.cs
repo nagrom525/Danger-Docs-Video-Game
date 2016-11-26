83e8dd74e58c24e36e3223336180260aca8137f3
elabraha <elabraha@umich.edu>
2016-11-26 04:44:31 -0500

    bucket stuff

---
+++ Assets/Scripts/BucketController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum BucketState { FULL, EMPTY };
+
+public class BucketController : MonoBehaviour {
+
+	private BucketState bstate;
+	private GameObject BucketObject;
+	private GameObject water;
+	public static BucketController BucketSingleton;
+
+	void Awake()
+	{
+		if (BucketSingleton == null)
+		{
+			BucketSingleton = this;
+		}
+		else
+		{
+			Debug.Log("Bucket can only be set once");
+		}
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.bstate = BucketState.EMPTY;
+		BucketObject = this.gameObject;
+		water = BucketObject.transform.GetChild(2).gameObject;
+		print(water.name);
+		water.SetActive(false);
+
+	}
+	void FillBucket()
+	{
+		this.bstate = BucketState.FULL;
+		water.SetActive(true);
+
+	}
+
+	BucketState getState()
+	{
+		return this.bstate;
+	}
+
+	void EmptyBucket()
+	{
+		this.bstate = BucketState.EMPTY;
+		water.SetActive(false);
+	}
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+}
---
+++ Assets/Scripts/BucketTool.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+//don't know what to do with this because you can't use it on the patient
+
+public class BucketTool : Tool {
+	public override ToolType GetToolType()
+	{
+		return ToolType.SUTURE;
+	}
+
+	// 
+	public override void OnDoctorInitatedInteracting()
+	{
+		print("OnDoctorInitatedInteracting was called");
+	}
+
+	public override void OnDoctorTerminatedInteracting()
+	{
+		throw new NotImplementedException();
+	}
+}
