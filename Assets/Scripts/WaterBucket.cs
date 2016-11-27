17c41d70b8447d483c4383e049cb95c8f4184249
Alex Chojnacki <thealex@umich.edu>
2016-11-26 20:05:57 -0500

    Added a basic bucket and script
    To eventually be updated with @elabraha's model

---
+++ Assets/Scripts/WaterBucket.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterBucket : Tool {
+	public bool hasWater {
+		get {return (waterLevel >= 1.0f);}
+	}
+
+	private float waterLevel;
+	private float waterGainRate;	
+	
+	public override ToolType GetToolType() {
+		return Tool.ToolType.BUCKET;
+	}
+
+	void Start() {
+		waterLevel = 0f;
+		waterGainRate = 0.25f;
+		originalMaterial = transform.GetComponentInChildren<Renderer>().material;
+	}
+
+	public void gainWater() {
+		print("We've gained water!");
+		waterLevel += waterGainRate * Time.deltaTime;
+		if (waterLevel > 1.0f) {
+			waterLevel = 1.0f;
+		}
+	}
+
+
+	public override void OnDoctorInitatedInteracting() {
+		return;
+	}
+	public override void OnDoctorTerminatedInteracting() {
+		return;
+	}
+
+
+}
