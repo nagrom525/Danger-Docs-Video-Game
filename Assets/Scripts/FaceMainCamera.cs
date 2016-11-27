5b6542e90963da656df5cef16e64d3bb1be61239
Alex Chojnacki <thealex@umich.edu>
2016-11-26 20:38:45 -0500

    Canvas above doctor head now looks at camera
    This keeps the gui above the doctor's head facing the main camera constantly.

---
+++ Assets/Scripts/FaceMainCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceMainCamera : MonoBehaviour {
+	public Camera targetCamera;
+
+	// Use this for initialization
+	void Start () {
+		targetCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(targetCamera.transform.position);
+	}
+}
