0ac82accb686119490561e4617407bf4880f98ab
Morgan Erlich <nagrom525@gmail.com>
2016-11-28 02:10:36 -0500

    Anesthetic machine guidence . Done

--- Assets/Scripts/CanisterContainer.cs
+++ Assets/Scripts/CanisterContainer.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class CanisterContainer : MonoBehaviour {
 
+    public GameObject actionButtonCanvas;
+    private bool anestheticMachineLowCalledOnce = false;
+
 	// Use this for initialization
 	void Start () {
-	
+        DoctorEvents.Instance.onAnestheticMachineLow += OnAnestheticMachineLow;
+        DoctorEvents.Instance.onAnestheticMachineReturned += OnAnestheticMachineReturned;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    private void OnAnestheticMachineLow(float precentLeft) {
+        if (!anestheticMachineLowCalledOnce) {
+            actionButtonCanvas.SetActive(true);
+        }
+        anestheticMachineLowCalledOnce = true;
+    }
+
+    private void OnAnestheticMachineReturned(float precentLeft) {
+        actionButtonCanvas.SetActive(false);
+    }
 }
