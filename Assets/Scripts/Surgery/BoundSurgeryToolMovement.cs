f618d7772ec5ab14cb48c3b82b830d8b1eec53aa
Alex Chojnacki <thealex@umich.edu>
2016-12-03 19:52:30 -0500

    bounded tool movement

--- Assets/Scripts/Surgery/BoundSurgeryToolMovement.cs
+++ Assets/Scripts/Surgery/BoundSurgeryToolMovement.cs
@@ -3,30 +3,24 @@ using System.Collections;
 
 public class BoundSurgeryToolMovement : MonoBehaviour {
 
-	public Bounds bounds;
-	public Vector3 boundsCenterOffset;
+	// Assuming p0 is the bottom left corner
+	// and p1 is the top right corner.
 
-	void Awake() {
-		Camera patientCamera = Patient.Instance.GetComponentInChildren<Camera> ();
-		Utils.SetCameraBounds (patientCamera);
-		// Then set them
-		bounds = Utils.CombineBoundsOfChildren(this.gameObject);
-		// Also find the diff between bounds.center & transform.position.
-		boundsCenterOffset = bounds.center - pos;
+	public GameObject p0;
+	public GameObject p1;
+
+	void Start() {
+		p0 = Patient.Instance.transform.Find("bound0").gameObject;
+		p1 = Patient.Instance.transform.Find("bound1").gameObject;
 	}
-		
-	void Update () {
-		// Check if tool is out of bounds and correct position.
-		// Calculate offset
-		Vector3 offset = Utils.ScreenBoundsCheck(bounds, BoundsTest.onScreen);
-		if (offset != Vector3.zero) {
-			print ("offset :: " + offset);
-			// Add offset to current position to prevent moving out of bounds.
-//			pos += offset;
-		}
 
-		// Update bounds
-		bounds.center = pos + boundsCenterOffset;
+	void Update() {
+
+		// We only need to worry about updating pos on the xz plane.
+		float x = Mathf.Clamp(pos.x, p0.transform.position.x, p1.transform.position.x);
+		float z = Mathf.Clamp(pos.z, p0.transform.position.z, p1.transform.position.z);
+
+		pos = new Vector3(x, pos.y, z);
 	}
 
 	public Vector3 pos {
