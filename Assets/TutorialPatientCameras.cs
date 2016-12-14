0df6170d563aac4830b708c6b5fadc4539a16954
Benjamin Lapid <benlap@umich.edu>
2016-12-14 16:52:53 -0500

    Tutorial patient cameras

--- Assets/TutorialPatientCameras.cs
+++ Assets/TutorialPatientCameras.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class TutorialPatientCameras : MonoBehaviour {
 	public GameObject[] cameras;
-
+	public LevelUserInterface lvlUI;
 	void Start () {
 		TutorialEventController.Instance.OnPickupToolsEnd += ShowCameras;
 		TutorialEventController.Instance.OnSurgeryOnPatientStart += ShowCameras;
@@ -13,14 +13,28 @@ public class TutorialPatientCameras : MonoBehaviour {
 
 	void ShowCameras()
 	{
+		lvlUI.SetHeartMonitorActive(false);
 		foreach (GameObject x in cameras)
 			x.SetActive(true);
 	}
 
 	void HideCameras()
 	{
+		lvlUI.SetHeartMonitorActive(true);
 		foreach (GameObject x in cameras)
 			x.SetActive(false);
+	}
+
+	/*
+	void OnEnable()
+	{
+		lvlUI.SetHeartMonitorActive(false);
+	}
+	*/
+
+	void OnDisable()
+	{
+		lvlUI.SetHeartMonitorActive(true);
 	}
 
 }
