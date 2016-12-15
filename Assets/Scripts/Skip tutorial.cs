705119ae2c64440e855188db000144b9b2253be8
elabraha <elabraha@umich.edu>
2016-12-15 01:44:59 -0500

    done with scene switch script

---
+++ Assets/Scripts/Skip tutorial.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Skiptutorial : MonoBehaviour {
+	string scenename = "";
+	// Use this for initialization
+	void Start () {
+		scenename = SceneManager.GetActiveScene().name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (scenename == "Tutorial")
+		{
+			if (Input.GetKeyDown(KeyCode.Alpha1))
+			{
+				SceneManager.LoadScene("Debug_Ben_2");
+			}
+		}
+
+	}
+}
