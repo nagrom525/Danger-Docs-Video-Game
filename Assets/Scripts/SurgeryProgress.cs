bae4d6576de5b8e8f2bcf99d0101b5fa58e6b99d
Morgan Erlich <nagrom525@gmail.com>
2016-12-15 18:21:56 -0500

    progressbar done

---
+++ Assets/Scripts/SurgeryProgress.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SurgeryProgress : MonoBehaviour {
+    public Image progressIndicator;
+    public float fillPrecentPerSecond = 20.0f;
+
+    private bool filling = false;
+    private float currentPrecentComplete;
+    private float newPrecentComplete;
+    private float fillStartTime;
+    private float totalFillTIme;
+
+    // Use this for initialization
+    void Start() {
+        DoctorEvents.Instance.onSurgeryOperationComplete += OnSurgeryComplete;
+        currentPrecentComplete = DoctorEvents.Instance.GetSurgeryPrecentComplete();
+        progressIndicator.fillAmount = currentPrecentComplete;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (filling) {
+            float t = (Time.time - fillStartTime) / totalFillTIme;
+            if(t >= 1.0) {
+                filling = false;
+                progressIndicator.fillAmount = newPrecentComplete;
+                currentPrecentComplete = newPrecentComplete;
+            }
+            progressIndicator.fillAmount = currentPrecentComplete + ((newPrecentComplete - currentPrecentComplete) * t);
+        }
+	}
+
+    private void OnSurgeryComplete(float duration) {
+        newPrecentComplete = DoctorEvents.Instance.GetSurgeryPrecentComplete();
+        fillStartTime = Time.time;
+        totalFillTIme = ((newPrecentComplete - currentPrecentComplete) * 100) / fillPrecentPerSecond;
+        filling = true;
+    }
+
+    void OnDestroy() {
+        DoctorEvents.Instance.onSurgeryOperationComplete -= OnSurgeryComplete;
+    }
+}
