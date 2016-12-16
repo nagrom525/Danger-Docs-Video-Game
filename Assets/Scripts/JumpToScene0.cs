f0f163acfba058d86757a370aad9f90613faf774
Morgan Erlich <nagrom525@gmail.com>
2016-12-16 17:30:36 -0500

    done

--- Assets/Scripts/JumpToScene0.cs
+++ Assets/Scripts/JumpToScene0.cs
@@ -4,17 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class JumpToScene0 : MonoBehaviour {
     public float TimeToJump = 5.0f;
-	// Use this for initialization
-	void Start () {
-        Invoke("JumpToScene0Func", TimeToJump);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private float startTime;
+    // Use this for initialization
+    void Start() {
+        startTime = Time.unscaledTime;
+    }
 
-    private void JumpToScene0Func() {
-        SceneManager.LoadScene(0);
+    // Update is called once per frame
+    void Update() {
+        if ((Time.unscaledTime - startTime) >= TimeToJump) {
+            SceneManager.LoadScene(0);
+        }
     }
 }
