fba913ef2e3144ad842b00e7284b9b2aef6f0e2d
elabraha <elabraha@umich.edu>
2016-12-15 22:26:04 -0500

    done

--- Assets/Scripts/GameLogicControlSystems/AudioControl.cs
+++ Assets/Scripts/GameLogicControlSystems/AudioControl.cs
@@ -38,6 +38,7 @@ public class AudioControl : MonoBehaviour {
 	public AudioClip tutorialBeginMusic;
 	public AudioClip tutorialLoopMusic;
 	public AudioClip dashCharge;
+	public AudioClip playAddTutorialCircle;
 
     private AudioSource mainGameMusicAudioSrc;
 	private AudioSource heartRateAudioSrc;
@@ -61,6 +62,7 @@ public class AudioControl : MonoBehaviour {
 	private AudioSource tutorialBeginMusicSrc;
 	private AudioSource tutorialLoopMusicSrc;
 	private AudioSource dashChargeSrc;
+	private AudioSource playAddTutorialCircleSrc;
 
     private static AudioControl _instance;
     public static AudioControl Instance {
@@ -103,6 +105,7 @@ public class AudioControl : MonoBehaviour {
 			tutorialBeginMusicSrc = sources[22];
 			tutorialLoopMusicSrc = sources[23];
 			dashChargeSrc = sources[24];
+			playAddTutorialCircleSrc = sources[25];
         }
 		//toolPickupAudioSrc.clip = toolPickup;
 		//toolPickupAudioSrc.Play();
