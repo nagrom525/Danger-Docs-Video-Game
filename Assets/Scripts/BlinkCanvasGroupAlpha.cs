a37bef223fa86b2d6f6cc0e8900423d1d7dec640
Benjamin Lapid <benlap@umich.edu>
2016-12-15 21:54:17 -0500

    Main Menu Scene

---
+++ Assets/Scripts/BlinkCanvasGroupAlpha.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkCanvasGroupAlpha : MonoBehaviour {
+
+    public CanvasGroup CG;
+    private float t;
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        CG = GetComponent<CanvasGroup>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        t += Time.deltaTime;
+        CG.alpha = Mathf.Sin(speed*t);
+	}
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+using UnityEngine.SceneManagement;
+public class MainMenuController : MonoBehaviour
+{
+
+    public int playerNum;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        InputDevice inputDevice = (InputManager.Devices.Count > playerNum) ? InputManager.Devices[playerNum] : null;
+        if (inputDevice == null)
+        {
+            // If no controller exists for this doctor make it translucent.
+            //playerRenderer.material.color = new Color(1.0f, 1.0f, 1.0f, 0.2f);
+        }
+        else
+        {
+            UpdateWithInputDevice(inputDevice);
+        }
+    }
+
+    void UpdateWithInputDevice(InputDevice inputDevice)
+    {
+        if (inputDevice.Action1.WasPressed)
+        {
+            //start game
+            SceneManager.LoadScene("Tutorial");
+   
+        }
+        else
+        if (inputDevice.Action2.WasPressed)
+        {
+
+
+        }
+        else
+        if (inputDevice.Action3.WasPressed)
+        {
+
+        }
+        else
+        if (inputDevice.Action4)
+        {
+        
+        }
+
+
+
+    }
+}
+
