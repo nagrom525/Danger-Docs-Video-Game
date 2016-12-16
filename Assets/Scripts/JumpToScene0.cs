fc27abea9de74c608b6b14499fa991af854a22da
Morgan Erlich <nagrom525@gmail.com>
2016-12-16 17:20:34 -0500

    fixed win and loose screens

---
+++ Assets/Scripts/JumpToScene0.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class JumpToScene0 : MonoBehaviour {
+    public float TimeToJump = 5.0f;
+	// Use this for initialization
+	void Start () {
+        Invoke("JumpToScene0Func", TimeToJump);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void JumpToScene0Func() {
+        SceneManager.LoadScene(0);
+    }
+}
